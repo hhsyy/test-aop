ff446b91571e8cb3d71c7cc1c1883d667a45873d
hhsyy <1062849557@qq.com>
2020-05-29 17:36:31 +0800

    springboot切面知识梳理

--- src/main/java/com/yiyuclub/testaop/aopconfig/UserAopOperator.java
+++ src/main/java/com/yiyuclub/testaop/aopconfig/UserAopOperator.java
@@ -3,10 +3,8 @@ package com.yiyuclub.testaop.aopconfig;
 import com.google.gson.Gson;
 import org.apache.tomcat.util.json.JSONParser;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -20,11 +18,34 @@ public class UserAopOperator {
 
     @Before("test()")
     public void doBefore(JoinPoint joinPoint)throws Exception{
+        System.out.println("切入点执行前执行");
         System.out.println(new Gson().toJson(joinPoint.getArgs()));
     }
 
+    @Around("test()")
+    public void around(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.out.println("切入点执行前执行");
+        joinPoint.proceed();//区分前后
+        System.out.println("切入点执行后执行");
+    }
+
+
     @After("test()")
     public void doAfter(JoinPoint joinPoint)throws Exception{
+        System.out.println("切入点执行后执行");
         System.out.println(new Gson().toJson(joinPoint.getArgs()));
     }
+
+    @AfterReturning("test()")
+    public void afterReturning(JoinPoint point) {
+        System.out.println("切入点执行返回结果后执行");
+    }
+
+    @AfterThrowing(value = "test()", throwing = "ex")
+    public void afterThrowing(Throwable ex) {
+        System.out.println("切入点执行异常后执行");
+        System.out.println("ex = " + ex.getMessage());
+    }
+
+
 }
